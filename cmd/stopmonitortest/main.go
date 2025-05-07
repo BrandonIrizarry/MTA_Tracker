63650bf1e048d095e84b58108ecfddf605bfc603
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-05-06 22:21:54 -0400

    Refactor GET call into its own package
    Some minor refactoring for consistency was also performed: namely,
    adding _all_ query parameters via the query map.

--- cmd/stopmonitortest/main.go
+++ cmd/stopmonitortest/main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"time"
 
+	"github.com/BrandonIrizarry/MTA_Tracker/internal/geturl"
 	"github.com/joho/godotenv"
 )
 
 const (
-	stopMonitoringBaseURL = "https://bustime.mta.info/api/siri/stop-monitoring.json?&version=2"
+	stopMonitoringBaseURL = "https://bustime.mta.info/api/siri/stop-monitoring.json"
 )
 
 func main() {
@@ -25,64 +22,18 @@ func main() {
 		log.Fatal("API_KEY environment variable not set")
 	}
 
-	stopMonitoringURL, err := initStopMonitoringURL(apiKey)
+	queries := map[string]string{
+		"key":           apiKey,
+		"version":       "2",
+		"LineRef":       "M11",
+		"MonitoringRef": "401386",
+	}
+
+	responseBytes, err := geturl.Call(stopMonitoringBaseURL, queries)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	queryValues := stopMonitoringURL.Query()
-
-	queryValues.Add("LineRef", "M11")
-	queryValues.Add("DirectionRef", "1")
-	queryValues.Add("MonitoringRef", "401386")
-
-	stopMonitoringURL.RawQuery = queryValues.Encode()
-
-	jsonDump, err := callURL(*stopMonitoringURL)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(jsonDump))
-}
-
-func callURL(url url.URL) ([]byte, error) {
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-	}
-
-	req, err := http.NewRequest("GET", url.String(), nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	res, err := client.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func initStopMonitoringURL(apiKey string) (*url.URL, error) {
-	apiKeyQuery := fmt.Sprintf("&key=%s", apiKey)
-	stopMonitoringURL, err := url.Parse(stopMonitoringBaseURL + apiKeyQuery)
-
-	if err != nil {
-		return &url.URL{}, err
-	}
-
-	return stopMonitoringURL, nil
+	fmt.Println(string(responseBytes))
 }
