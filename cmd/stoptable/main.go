57019ea4c83182c588fed84b04dcd628e84a3a16
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-05-09 21:30:26 -0400

    Lay groundwork for supporting flags + don't clear db
    Functionality is beginning to be refactored into separate methods and
    files here. In particular, I'm close to adding CLI flag
    configurability (that is, the user will for now be able to select bus
    lines they'd like to add to, or remove from, the database.)
    
    The one "breaking change" here is that the database is no longer
    cleared on each call. This is simply because we want our added bus
    stops to persist across calls now.

--- cmd/stoptable/main.go
+++ cmd/stoptable/main.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/BrandonIrizarry/MTA_Tracker/cmd/stoptable/internal/database"
 	"github.com/BrandonIrizarry/MTA_Tracker/cmd/stoptable/internal/onebusaway"
 	"github.com/BrandonIrizarry/MTA_Tracker/internal/geturl"
 	"github.com/joho/godotenv"
+	"github.com/spf13/pflag"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -28,60 +31,79 @@ const (
 	// tableDBName is the name of the SQLite table where the
 	// id-name associations will be stored and queried from.
 	tableDBName = "./cmd/stoptable/stoptable.db"
-
-	// For the moment, this is hardcoded. Later, I'll make this
-	// configurable via a command-line flag.
-	routeID = "MTA NYCT_M11"
 )
 
+type config struct {
+	apiKey    string
+	dbQueries *database.Queries
+}
+
+type routeList []string
+
+func (rlist *routeList) Set(value string) error {
+	if len(*rlist) > 0 {
+		return errors.New("interval flag already set")
+	}
+
+	for _, route := range strings.Split(value, ",") {
+		*rlist = append(*rlist, route)
+	}
+
+	return nil
+}
+
+func (rlist *routeList) Type() string {
+	return "routeList"
+}
+
+func (rlist *routeList) String() string {
+	return fmt.Sprint(*rlist)
+}
+
+// Flags.
+var newRoutes routeList
+var obsoleteRoutes routeList
+
 func main() {
-	godotenv.Load(".env")
+	// See which routes we're adding and/or removing.
+	pflag.Parse()
 
-	apiKey := os.Getenv("API_KEY")
-
-	if apiKey == "" {
-		log.Fatal("API_KEY environment variable not set")
-	}
-
-	queries := map[string]string{
-		"key":              apiKey,
-		"version":          "2",
-		"includePolylines": "false",
-	}
-
-	stopsForRouteBaseURLFilled := fmt.Sprintf(stopsForRouteBaseURL, routeID)
-
-	responseBytes, err := geturl.Call(stopsForRouteBaseURLFilled, queries)
+	cfg, err := initConfig()
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	err = addRoute(cfg, "MTA NYCT_M11")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addRoute(cfg config, routeID string) error {
+	stopsForRouteBaseURLFilled := fmt.Sprintf(stopsForRouteBaseURL, routeID)
+
+	responseBytes, err := geturl.Call(stopsForRouteBaseURLFilled, map[string]string{
+		"key":              cfg.apiKey,
+		"version":          "2",
+		"includePolylines": "false",
+	})
+
+	if err != nil {
+		return err
 	}
 
 	// Marshall the response bytes into a OneBusAway struct.
 	var data onebusaway.OneBusAway
 
 	if err := json.Unmarshal(responseBytes, &data); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if code := data.Code; code != 200 {
-		log.Fatalf("OBA response reported '%d' response code, aborting", code)
+		return fmt.Errorf("OBA response reported '%d' response code, aborting", code)
 	}
-
-	// Start handling database-editing logic here.
-	db, err := sql.Open("sqlite3", tableDBName)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dbQueries := database.New(db)
-
-	// Before compiling the table inside the database, we should
-	// clear anything in it first. In the course of debugging and
-	// testing, we will be compiling the table afresh many, many
-	// times.
-	dbQueries.ClearAllStops(context.Background())
 
 	for _, stop := range data.Data.References.Stops {
 		stopParams := database.CreateStopParams{
@@ -90,8 +112,41 @@ func main() {
 			RouteID: routeID,
 		}
 
-		if err := dbQueries.CreateStop(context.Background(), stopParams); err != nil {
+		if err := cfg.dbQueries.CreateStop(context.Background(), stopParams); err != nil {
 			log.Fatal(err)
 		}
 	}
+
+	return nil
+}
+
+func initConfig() (config, error) {
+	apiKey := os.Getenv("API_KEY")
+
+	if apiKey == "" {
+		return config{}, errors.New("API_KEY environment variable not set")
+	}
+
+	db, err := sql.Open("sqlite3", tableDBName)
+
+	if err != nil {
+		return config{}, fmt.Errorf("Couldn't open database file '%s'\n", tableDBName)
+	}
+
+	dbQueries := database.New(db)
+
+	cfg := config{
+		apiKey:    apiKey,
+		dbQueries: dbQueries,
+	}
+
+	return cfg, nil
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	godotenv.Load(".env")
+
+	pflag.VarP(&newRoutes, "add", "a", "Add a route")
+	pflag.VarP(&obsoleteRoutes, "remove", "r", "Remove a route")
 }
